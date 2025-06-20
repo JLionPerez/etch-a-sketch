ae4205568c84a0645d82a3bf0e11ef67becc1a7d
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-20 16:14:52 -0700

    Add button and popup

--- sketch.js
+++ sketch.js
@@ -1,21 +1,27 @@
-const container = document.querySelector(".container")
-const canvas = document.createElement("div")
-canvas.setAttribute("class", "canvas")
-container.appendChild(canvas)
+const container = document.querySelector(".container");
+
+const button = document.createElement("button");
+button.textContent = "Resize Grid";
+container.appendChild(button);
+
+const canvas = document.createElement("div");
+canvas.setAttribute("class", "canvas");
+container.appendChild(canvas);
+
+let size = 16;
 
 let createGrid = (num) => {
     for (let i = 0; i < num; i++) { 
         const row = document.createElement("div");
-        row.setAttribute("class","row")
+        row.setAttribute("class","row");
         for(let j = 0; j < num; j++) {
             const cell = document.createElement('div');
             cell.setAttribute("class","cell");
             colorOver(cell);
             row.appendChild(cell);
         }
-    canvas.appendChild(row)
+    canvas.appendChild(row);
     }
-
 }
 
 let colorOver = (obj) => {
@@ -24,4 +30,17 @@ let colorOver = (obj) => {
     });
 }
 
-createGrid(100)
+createGrid(size)
+
+button.addEventListener("click", (event) => {
+    alert(size = parseInt(prompt("Size of new grid: ")))
+    if (size > 100) {
+        alert("You cannot exceed a size of 100 cells.")
+    }
+    if (size <= 100) {
+        while (canvas.firstChild) {
+            canvas.removeChild(canvas.firstChild);
+        }
+        createGrid(size)
+    }
+})
